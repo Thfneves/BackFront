9cf34715d65de59fc46b6832562692e37952f279
Thfneves <Thiago_neves1997@hotmail.com>
2024-07-29 21:15:08 -0300

    Consolidando estudos de classe

--- Classe/Classe/main.swift
+++ Classe/Classe/main.swift
@@ -13,28 +13,48 @@ class Automovel {
     var marca: String = "Honda"
     var eletrico: Bool = false
     var quantidadeDePassageiros: Int = 5
-    
+    var velocidadeTotal: Int = 100
+}
     func ligarFarol() {
         print("O Farol está ligado")
     }
     func velocidade () {
-        print("0 carro está a ",velocidade, "por hora")
+        print("0 carro está a por hora")
         
         
     }
     
-}
-class rua {
-    var faixa: String = "Faixa pedestre"
-    var placa: String = "Pare"
     
-}
-class semaforo{
-    var vermelho: String = "Vermelho"
-    var verde: String = "Verde"
-    var laranha: String = "Laranja"
-}
+    class rua {
+        var faixa: String = "Faixa pedestre"
+        var placa: String = "Pare"
+        
+    }
+    class semaforo{
+        var vermelho: String = "Vermelho"
+        var verde: String = "Verde"
+        var laranha: String = "Laranja"
+    }
+    
+    class velocidades {
+        var velocidade: Int = 50
+        
+    }
+    //Construtores
+    class Pessoa{
+        var nome: String
+        var altura: Double
+        var peso: Double
+        
+        init (nome: String, altura: Double, peso: Double) {
+            self.nome = nome
+            self.altura = altura
+            self.peso = peso
+        } 
+    }
+    
+    
 
-class velocidade {
-    var velocidade: Int = 50
-}
+        
+var caio: Pessoa = Pessoa(nome:"Thiago", altura:1.81, peso:85)
+ 
