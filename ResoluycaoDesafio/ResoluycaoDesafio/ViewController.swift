abe6232019525104dcf05dc58a16b41c2b08b9a9
Thfneves <Thiago_neves1997@hotmail.com>
2024-09-17 20:30:27 -0300

    no message

---
+++ ResoluycaoDesafio/ResoluycaoDesafio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ResoluycaoDesafio
+//
+//  Created by Thiago Neves on 17/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ResoluycaoDesafio/ResoluycaoDesafioTests/ResoluycaoDesafioTests.swift
@@ -0,0 +1,17 @@
+//
+//  ResoluycaoDesafioTests.swift
+//  ResoluycaoDesafioTests
+//
+//  Created by Thiago Neves on 17/09/24.
+//
+
+import Testing
+@testable import ResoluycaoDesafio
+
+struct ResoluycaoDesafioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
